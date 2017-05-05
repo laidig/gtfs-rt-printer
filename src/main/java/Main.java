7a97d405c574745091bc13674e60eafbba9f2a1a
Tony Laidig <laidig@gmail.com>
2017-05-05 18:31:56 -0400

    now works on either files or urls

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,22 +1,39 @@
 import com.google.transit.realtime.*;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 
 public class Main {
+    static URL url;
     public static void main(String[] args){
 
         // Load the file
         InputStream stream = null;
-        try{
-            File feed = new File("src/main/resources/bart-gtfs-rt.pb");
-            stream = new FileInputStream(feed);
-        } catch (Exception e) {
-            e.printStackTrace();
-            System.exit(-1);
+        System.out.println("loading " + args[0]);
+
+        if (args[0].startsWith("http")){
+            try {
+                url = new URL(args[0]);
+                stream = url.openStream();
+            } catch (MalformedURLException e) {
+                System.out.println("problem with URL " + args[0]);
+            } catch (IOException e){
+                e.printStackTrace();
+            }
+        } else {
+
+            try {
+                File feed = new File(args[0]);
+                stream = new FileInputStream(feed);
+            } catch (Exception e){
+                System.out.println("Could not load file " + args[0]);
+            }
         }
+
 
         GtfsRealtime.FeedMessage feedMessage =null;
         // Parse file into an object in memory
